9e7af5484e3492898772329602c4aac0f0ac4ca0
Ataev.m.N <104029906+Brifff@users.noreply.github.com>
2023-03-20 11:47:50 +0300

    drive init

---
+++ Godunov Drive/Godunov Drive/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  Godunov drive
+//
+//  Created by Годунов Антон Иванович on 16/1/2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+   
+}
